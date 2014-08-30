f7d50daa1888f4467e54930b8b4f32e15baab481
Luca Bruno <lucabru@src.gnome.org>
2014-08-30 13:11:51 +0200

    nix deployment

--- default.nix
+++ default.nix
@@ -28,51 +28,50 @@ let
 
 in
 
-{ 
-  url ? "http://localhost:8080", postField ? "content",
-  bindAddress ? "", bindPort ? 8080, staticDir ? ./static,
+{ url ? "http://localhost:8080", postField ? "text",
+  bindAddress ? "", bindPort ? 8080,
+  static ? ./static, views ? ./views,
   storageDir ? "/tmp/nixpaste", maxBytes ? 1000000, maxFiles ? 1000,
   hashSalt ? "somesalt", hashLength ? 6,
   extraTemplateVars ? {}
 }:
 
-  let
+assert hashSalt != null;
+assert hashLength > 4;
 
-    opts = { inherit url postField bindAddress bindPort staticDir storageDir maxBytes maxFiles hashSalt hashLength; };
+let
 
-    static = pkgs.runCommand "nixpaste-static"
-      (opts // extraTemplateVars) ''
-      mkdir -p $out
-      cp -r $staticDir/* $out/
-    '';
-    
-    configpy = pkgs.writeText "nixpaste-config" ''{
-      "STATIC": "${static}",
-      "URL": "${url}",
-      "POST_FIELD": "content",
-      "BIND": "${bindAddress}",
-      "PORT": ${builtins.toString 8080},
-      "DIR": "${storageDir}",
-      "MAX_BYTES": ${builtins.toString maxBytes},
-      "MAX_FILES": ${builtins.toString 3},
-      "SALT": "${hashSalt}",
-      "LENGTH": ${builtins.toString hashLength}
-    }'';
-
-  in
+  opts = { inherit url postField bindAddress bindPort static views storageDir maxBytes maxFiles hashSalt hashLength; };
+  
+  configjson = pkgs.writeText "nixpaste-config" ''{
+    "STATIC": "${static}",
+    "VIEWS": "${views}",
+    "URL": "${url}",
+    "POST_FIELD": "${postField}",
+    "BIND": "${bindAddress}",
+    "PORT": ${builtins.toString 8080},
+    "DIR": "${storageDir}",
+    "MAX_BYTES": ${builtins.toString maxBytes},
+    "MAX_FILES": ${builtins.toString 3},
+    "SALT": "${hashSalt}",
+    "LENGTH": ${builtins.toString hashLength}
+  }'';
+  
+in
   
   stdenv.mkDerivation {
     name = "nixpaste";
-
-    buildInputs = [ python pygments2 webapp2 makeWrapper ];
-
+    
+    buildInputs = [ python2 pygments2 webapp2 makeWrapper ];
+    
     buildCommand = ''
       mkdir -p $out/bin
-      cp ${./nixpaste.py} $out/bin/nixpaste
+      mkdir -p $out/python-lib
+      cp -pdv ${./nixpaste.py} $out/bin/nixpaste
+      ln -s ${./bottle.py} $out/python-lib/bottle.py
       wrapProgram $out/bin/nixpaste \
-        --prefix PYTHONPATH : $PYTHONPATH \
-        --set NIXPASTE_CONFIG ${configpy}
+        --prefix PYTHONPATH : "$out/python-lib:$PYTHONPATH" \
+        --set NIXPASTE_CONFIG ${configjson}
       patchShebangs $out
     '';
   }
-
